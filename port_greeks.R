8ee9fcdf4d25020bbe7a2d9f509d77731405574a
Antonio Minondo <aminondo@nd.edu>
2017-06-21 14:31:15 +0200

    progress in building table

--- port_greeks.R
+++ port_greeks.R
@@ -2,7 +2,6 @@ library(dplyr)
 library(readr)
 
 opt = read_csv("Jun14 opt.csv")
-View(opt)
 
 opt$Ticker = strsplit(opt$Symbol, " ") %>% sapply(function(x){
   x[[1]]
@@ -23,6 +22,7 @@ opt = mutate(opt, T.Theta=opt$Quantity*opt$Theta*contract_size)
 opt = mutate(opt, T.Gamma=opt$Quantity*opt$Gamma*contract_size)
 opt = mutate(opt, T.Vega=opt$Quantity*opt$Vega*contract_size)
 opt = mutate(opt, T.Rho=opt$Quantity*opt$Rho*contract_size)
+length(opt)
 opt = opt[c(1,8,2,3,4,5,6,7,9:14)] #reorder columns
 
 write_csv(opt,"opt.csv")
--- risk_analysis.R
+++ risk_analysis.R
@@ -2,8 +2,13 @@ library(dplyr)
 library(readr)
 library(quantmod)
 library(tidyr)
+
+
+#GLOBALS
+contract_size=100
+#-------------------------------------------------------------------------------------
+
 opt = read_csv("opt.csv")
-View(opt)
 
 
 tickers = unique(opt$Ticker)
@@ -16,9 +21,24 @@ quotes2 = sapply(tickers, function(x){
 cts = opt %>% group_by(Ticker,Type) %>% summarize(aggr=sum(Quantity)) #filter(opt,Ticker=="AAPL",Type=="Put")
 cts = spread(cts,Type,aggr)
 
-tbl = data.frame(tickers,spot=quotes2)
-tbl = cbind(tbl,cts_P = cts[[3]],cts_C= cts[[2]]) #add aggregates to tbl
+tbl = data.frame(Ticker=tickers,spot=quotes2)
+tbl = inner_join(cts,tbl,by="Ticker")
+tbl = tbl[c(1,4,2,3)] #reorder columns
+#rename cols
+tbl = rename(tbl,cts_C=Call)
+tbl = rename(tbl,cts_P=Put)
+tbl$cts_Net = tbl$cts_P-tbl$cts_C #add col
 
-tbl$cts_Net = tbl$cts_P-tbl$cts_C
+#get aggregate greeks
+greeks = opt %>% group_by(Ticker) %>% summarize(Delta=sum(T.Delta),Gamma=sum(T.Gamma),Theta=sum(T.Theta),Vega=sum(T.Vega))
+tbl = inner_join(tbl,greeks, by="Ticker")
 
-View(opt)
+# %Delta
+tbl$`%_Delta` = tbl$Delta * tbl$spot / contract_size
+
+# %_Gamma
+tbl$`%_Gamma` = tbl$Gamma * tbl$spot / contract_size
+
+# % of port delta
+total_delta = sum(tbl$Delta)
+tbl$`%_of_Port_Delta` = tbl$Delta/total_delta*100
