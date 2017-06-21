81893fd45b045a8fecbdcad64cada4ede17ad8d3
Antonio Minondo <aminondo@nd.edu>
2017-06-21 13:33:26 +0200

    building table for risk analysis

--- risk_analysis.R
+++ risk_analysis.R
@@ -1,14 +1,24 @@
 library(dplyr)
 library(readr)
-
-opt = read_csv("Jun14 opt.csv")
+library(quantmod)
+library(tidyr)
+opt = read_csv("opt.csv")
 View(opt)
 
-strsplit(opt$Symbol, " ") %>% sapply(function(x){
-  x[[1]]
+
+tickers = unique(opt$Ticker)
+#find quotes of tickers
+quotes2 = sapply(tickers, function(x){
+  getQuote(x)$Last
 })
 
-opt$sym = strsplit(opt$Symbol, " ") %>% sapply(function(x){
-  x[[1]]
-})
+#aggregate quantities by type of option
+cts = opt %>% group_by(Ticker,Type) %>% summarize(aggr=sum(Quantity)) #filter(opt,Ticker=="AAPL",Type=="Put")
+cts = spread(cts,Type,aggr)
 
+tbl = data.frame(tickers,spot=quotes2)
+tbl = cbind(tbl,cts_P = cts[[3]],cts_C= cts[[2]]) #add aggregates to tbl
+
+tbl$cts_Net = tbl$cts_P-tbl$cts_C
+
+View(opt)
