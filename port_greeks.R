5706b79142dd9d0902cddb6dca5c73730839f97a
Antonio Minondo <aminondo@nd.edu>
2017-06-21 12:45:58 +0200

    port greeks now creates csv

--- port_greeks.R
+++ port_greeks.R
@@ -21,3 +21,5 @@ opt = mutate(opt, T.Gamma=opt$Quantity*opt$Gamma*contract_size)
 opt = mutate(opt, T.Vega=opt$Quantity*opt$Vega*contract_size)
 opt = mutate(opt, T.Rho=opt$Quantity*opt$Rho*contract_size)
 opt = opt[c(1,8,2,3,4,5,6,7,9:14)] #reorder columns
+
+write_csv(opt,"opt.csv")
