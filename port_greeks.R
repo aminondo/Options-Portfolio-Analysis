194400cfdaabd5c3ce6875af12b7fb2ff1657f9f
Antonio Minondo <aminondo@nd.edu>
2017-06-22 12:52:38 +0200

    now opt file gets spots instead of other files doing it aswell

--- port_greeks.R
+++ port_greeks.R
@@ -1,14 +1,21 @@
 library(dplyr)
 library(readr)
-
+#read in file
 opt = read_csv("Jun14 opt.csv")
+#GLOBALS------------------------------------------------------------------
+contract_size=100
+#-------------------------------------------------------------------------
 
+#generate Ticker and Type columns
 opt$Ticker = strsplit(opt$Symbol, " ") %>% sapply(function(x){
   x[[1]]
 })
+
 opt$Type = strsplit(opt$Symbol, " ") %>% sapply(function(x){
   x[[6]]
 })
+
+#clean columns I don't need
 opt$`Symbol Description`=NULL
 opt$`Last Trade`=NULL
 opt$`$ Chg. Close`=NULL
@@ -16,13 +23,25 @@ opt$Bid = NULL
 opt$Ask=NULL
 opt$`IV %`=NULL
 
-contract_size=100
+#get unique tickers and get spots
+tickers = unique(opt$Ticker)
+quotes = sapply(tickers, function(x){
+  getQuote(x)$Last
+})
+quotes=data.frame(Ticker=tickers,Spot=quotes)
+#adding needed columns
 opt = mutate(opt, T.Delta=opt$Quantity*opt$Delta*contract_size)
 opt = mutate(opt, T.Theta=opt$Quantity*opt$Theta*contract_size)
 opt = mutate(opt, T.Gamma=opt$Quantity*opt$Gamma*contract_size)
 opt = mutate(opt, T.Vega=opt$Quantity*opt$Vega*contract_size)
 opt = mutate(opt, T.Rho=opt$Quantity*opt$Rho*contract_size)
-length(opt)
 opt = opt[c(1,8,2,3,4,5,6,7,9:14)] #reorder columns
+
+#Forumla for Spot
+for(id in 1:nrow(opt)){
+  val = quotes[quotes$Ticker==opt[id,]$Ticker,]$Spot
+  print(val)
+  #opt[id,]$Spot = val
+}
 
 write_csv(opt,"opt.csv")
