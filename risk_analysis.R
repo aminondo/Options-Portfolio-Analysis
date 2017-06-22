a88bf06b9f1b0dec89e737be30cea780d17edd96
Antonio Minondo <aminondo@nd.edu>
2017-06-22 13:02:58 +0200

    now risk analysis table gets spots from opt

--- risk_analysis.R
+++ risk_analysis.R
@@ -1,3 +1,12 @@
+# Author: Antonio Minondo
+# File: risk_analysis.R
+# Description: This file generates risk_analysis table for options portolio. Uses file
+# opt.csv downloaded from Etrade Api in order to fill the columns
+# Current file contains information on the folowing options:
+# "AAPL" "BA"   "BLK"  "BUD"  "CMG"  "CVX"  "FB"   "GS"   "IBM"  "JNJ"  "JPM"  "M"    "MAR" 
+# "MCD"  "MMM"  "NFLX" "NVDA" "PXD"  "SPY"  "UAL"  "UNP"  "WYNN"
+# Date: June 22, 2017
+# ----------------------------------------------------------------------------------------------
 library(dplyr)
 library(readr)
 library(quantmod)
@@ -14,41 +23,50 @@ margin_return = read_csv("margin_return.csv")
 
 # -----------------------------------------------------------------------------------
 
+#get unique quotes and tickers
 tickers = unique(opt$Ticker)
-#find quotes of tickers
-quotes2 = sapply(tickers, function(x){
-  getQuote(x)$Last
-})
+quotes = opt %>% group_by(Ticker) %>% summarize(Spot = unique(Spot))
 
 #aggregate quantities by type of option
-cts = opt %>% group_by(Ticker,Type) %>% summarize(aggr=sum(Quantity)) #filter(opt,Ticker=="AAPL",Type=="Put")
+cts = opt %>% group_by(Ticker,Type) %>% summarize(aggr=sum(Quantity))
 cts = spread(cts,Type,aggr)
+#replace 0s before joining main tbl
+cts = cts %>% replace_na(list(Call = 0,Put=0))
 
-tbl = data.frame(Ticker=tickers,spot=quotes2)
+#create main tbl
+tbl = data.frame(Ticker=tickers,Spot=quotes$Spot)
 tbl = inner_join(tbl,cts,by="Ticker")
-#tbl = tbl[c(1,4,2,3)] #reorder columns
 #rename cols
 tbl = dplyr::rename(tbl,cts_C=Call)
 tbl = dplyr::rename(tbl,cts_P=Put)
+
 tbl$cts_Net = tbl$cts_P-tbl$cts_C #add col
 
 #Notional
 notional = margin_return %>% group_by(Ticker,Type) %>% summarize(aggr = sum(Notional))
-View(notional)
 notional = spread(notional,Type,aggr)
+#replace 0s before joining main tbl
+notional = notional %>% replace_na(list(Call = 0,Put=0))
+#merging notional...
 tbl = inner_join(tbl,notional, by="Ticker")
+#renaming columns
 tbl = dplyr::rename(tbl,Notional_C=Call)
 tbl = dplyr::rename(tbl,Notional_P=Put)
 
 #get aggregate greeks
 greeks = opt %>% group_by(Ticker) %>% summarize(Delta=sum(T.Delta),Gamma=sum(T.Gamma),Theta=sum(T.Theta),Vega=sum(T.Vega))
+
+#no NAs should be in this file, but for robustness sake, remove na's before joining
+#main tbl
+greeks = greeks %>% replace_na(list(Delta = 0,Gamma=0,Theta=0,Vega=0))
+#merging greeks...
 tbl = inner_join(tbl,greeks, by="Ticker")
 
 # %Delta
-tbl$`%_Delta` = tbl$Delta * tbl$spot / contract_size
+tbl$`%_Delta` = tbl$Delta * tbl$Spot / contract_size
 
 # %_Gamma
-tbl$`%_Gamma` = tbl$Gamma * tbl$spot / contract_size
+tbl$`%_Gamma` = tbl$Gamma * tbl$Spot / contract_size
 
 # % of port delta
 total_delta = sum(tbl$Delta)
@@ -80,3 +98,6 @@ tbl$log_roll = tbl$`%_Gamma`/tbl$`%_Delta`
 
 #theta_gamma_cvg
 tbl$theta_gamma_cvg = -tbl$Theta/tbl$`%_Gamma`
+
+
+
